b3028fca30ffa0d2ec8ca8a4cc5d683c40227246
algabl <103858110+algabl@users.noreply.github.com>
2024-02-27 14:16:25 -0700

    some more eda, added a path file for each to use on our local machines

--- eda.r
+++ eda.r
@@ -2,10 +2,15 @@ library(tidyverse)
 library(dplyr)
 library(splitstackshape)
 
-education <- read_csv('~/rstudio/is555milestone1/07_train.csv')
+# q: how could i reference an external file that contains my working directory?
+# a: i could use the source() function to reference an external file that contains my working directory
+path <- readLines("path.txt")
+
+setwd(path)
+
+education <- read_csv('07_train.csv')
 
 education <- education %>% 
-   
   janitor::clean_names()
 
 education %>% 
@@ -16,4 +21,21 @@ education %>%
 education %>% 
   filter(curricular_units_2nd_sem_credited > curricular_units_2nd_sem_approved) %>% 
   select(curricular_units_2nd_sem_credited, curricular_units_2nd_sem_approved)
+
+education |> 
+  # select where it contains credited
+  select(starts_with("curricular_units_")) |> 
+  pivot_longer(cols = everything(), names_to = "name", values_to = "units") |> 
+  ggplot(aes(x = units)) +
+  geom_density() +
+    facet_wrap(~name, scales = "free")
+
+education |> 
+  select(starts_with("curricular_units_")) |> 
+  # get summary stats (mean, median, etc) on each of these columns
+  summary()
+
+education |> 
+  filter(curricular_units_1st_sem_without_evaluations > 0 | curricular_units_2nd_sem_without_evaluations > 0) |> 
+  summary()
   
